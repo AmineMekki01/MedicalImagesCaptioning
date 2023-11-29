55e17f4f2b08bdf844436feea2dd715a1a890527
Amine MEKKI <amine.mekki@mines-ales.org>
2023-11-29 01:48:26 +0100

    adding image augmentation functions

---
+++ src/component/imageTransform.py
@@ -0,0 +1,23 @@
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+
+
+sample_tfms = [
+    A.HorizontalFlip(),
+    A.RandomBrightnessContrast(),
+    A.ColorJitter(),
+    A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.3, rotate_limit=45, p=0.5),
+    A.HueSaturationValue(p=0.3),
+]
+train_tfms = A.Compose([
+    *sample_tfms,
+    A.Resize(224,224),
+    A.Normalize(mean=[0.5,0.5,0.5],std=[0.5,0.5,0.5],always_apply=True),
+    ToTensorV2()
+])
+valid_tfms = A.Compose([
+    A.Resize(224,224),
+    A.Normalize(mean=[0.5,0.5,0.5],std=[0.5,0.5,0.5],always_apply=True),
+    ToTensorV2()
+])

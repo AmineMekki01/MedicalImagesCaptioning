3ba6c4410c1aa98dc922e5954aa8a1e1aff9ebc9
Amine MEKKI <amine.mekki@mines-ales.org>
2023-11-29 09:44:50 +0100

    adding configurations for training

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,7 +1,7 @@
 from src.constants import *
 from src.utils.commonFunctions import read_yaml, create_directories
 
-from src.entity.configEntity import DataProcessingConfig
+from src.entity.configEntity import DataProcessingConfig, TrainingConfig, ModelConfig
 
 
 class ConfigurationManager:
@@ -20,3 +20,35 @@ class ConfigurationManager:
         ) 
         return data_processor_config
     
+    def get_training_config(self) -> TrainingConfig:
+        config = self.config.train_config
+        create_directories([Path(config.trained_model_output_path), Path(config.metrics_folder_path)])
+        training_config = TrainingConfig(
+            training_data_path = Path(config.training_data_path),
+            trained_model_output_path = Path(config.trained_model_output_path),
+            metrics_folder_path = Path(config.metrics_folder_path),
+            metrics_path = Path(config.metrics_path),
+            batch_size = config.batch_size,
+            epochs = config.epochs,
+            learning_rate = config.learning_rate,
+            freeze_epochs_gpt = config.freeze_epochs_gpt,
+            freeze_epochs_all = config.freeze_epochs_all,
+            device = config.device
+        )
+        return training_config
+    
+    def get_model_config(self) -> ModelConfig:
+        config = self.config.model_config
+        model_config = ModelConfig(
+            vocab_size= config.vocab_size,
+            embed_dim= config.embed_dim,
+            num_heads= config.num_heads,
+            seq_len= config.seq_len,
+            depth= config.depth,
+            attention_dropout= config.attention_dropout,
+            residual_dropout= config.residual_dropout,
+            mlp_ratio= config.mlp_ratio,
+            mlp_dropout= config.mlp_dropout,
+            emb_dropout= config.emb_dropout
+        )
+        return model_config

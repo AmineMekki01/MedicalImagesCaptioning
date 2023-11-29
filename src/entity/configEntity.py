99c43b0af6a327ce1a647ac2056f9c1cab1c79ab
Amine MEKKI <amine.mekki@mines-ales.org>
2023-11-29 09:44:50 +0100

    adding configurations for training

--- src/entity/configEntity.py
+++ src/entity/configEntity.py
@@ -9,3 +9,29 @@ class DataProcessingConfig:
     projections_data_path: str
     reports_data_path: Path
     processed_data_output_path: Path
+
+@dataclass(frozen=True)
+class TrainingConfig:
+    training_data_path: Path
+    trained_model_output_path: Path
+    metrics_folder_path: Path
+    metrics_path: Path
+    batch_size: int
+    epochs: int
+    learning_rate: float
+    freeze_epochs_gpt: int
+    freeze_epochs_all: int  
+    device: str
+    
+@dataclass(frozen=True) 
+class ModelConfig:
+    vocab_size: int
+    embed_dim: int
+    num_heads: int
+    seq_len: int
+    depth: int
+    attention_dropout: float
+    residual_dropout: float
+    mlp_ratio: int
+    mlp_dropout: float
+    emb_dropout: float
--- src/utils/commonFunctions.py
+++ src/utils/commonFunctions.py
@@ -8,10 +8,12 @@ In this file i will put the common used functions so as i dont repeat my self.
 
 import os
 import yaml
+import pandas as pd
 from ensure import ensure_annotations
 from box.exceptions import BoxValueError
 from pathlib import Path
 from box import ConfigBox
+from sklearn.model_selection import train_test_split
 
 from src.logs import logger
 
@@ -64,4 +66,27 @@ def create_directories(path_to_directories : list, verbose : bool = True):
         os.makedirs(path, exist_ok=True)
         if verbose:
             logger.info(f"created directory at : {os.path.normpath(path)}")
-            
+        
+        
+@ensure_annotations
+def split_data(data : pd.DataFrame, validation_size : float = 0.1, test_size : float = 0.1, random_state : int = 42):
+    """
+    This function splits data into train and test sets.
+    
+    Parameters
+    ----------
+    data : list
+        list of data to split.
+    train_size : float, optional
+        train set size, by default 0.8
+    random_state : int, optional
+        random state, by default 42
+    
+    Returns
+    -------
+    train, test : list, list
+        train and test sets.
+    """
+    train, test = train_test_split(data, test_size=test_size, random_state=random_state)
+    train, validation = train_test_split(train, test_size=validation_size, random_state=random_state)
+    return train, validation, test  

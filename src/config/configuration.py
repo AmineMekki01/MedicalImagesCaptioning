2befe52fda6570840fbc61bc51bbb324009b376d
Amine MEKKI <amine.mekki@mines-ales.org>
2023-12-03 00:00:35 +0100

    updating configuration for the project

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,42 +1,83 @@
 from src.constants import *
 from src.utils.commonFunctions import read_yaml, create_directories
 
-from src.entity.configEntity import DataProcessingConfig, TrainingConfig, ModelConfig, InferenceConfig
+from src.entity.configEntity import ChestXrayDataProcessingConfig, ROCODataProcessingConfig, TrainingConfig, ChestXrayTrainingConfig, ROCOTrainingConfig, ModelConfig, InferenceConfig
 
 
 class ConfigurationManager:
     def __init__(self, config_filepath=CONFIG_FILE_PATH):
         self.config = read_yaml(config_filepath)
 
-    def get_data_processor_config(self) -> DataProcessingConfig:
-        config = self.config.data_processing_config
-        create_directories([Path(config.processed_data_path)])
-        data_processor_config = DataProcessingConfig(
-            processed_data_path=Path(config.processed_data_path),
-            images_base_path=Path(config.images_base_path),
-            projections_data_path=Path(config.projections_path),
-            reports_data_path=Path(config.reports_path),
-            processed_data_output_path=Path(config.processed_data_output_path)
+    def get_ChestXray_data_processor_config(self) -> ChestXrayDataProcessingConfig:
+        config_ChestXray = self.config.ChestXray_data_processing_config
+        create_directories([Path(config_ChestXray.processed_data_path)])
+        data_processor_config = ChestXrayDataProcessingConfig(
+            processed_data_path=Path(config_ChestXray.processed_data_path),
+            images_base_path=Path(config_ChestXray.images_base_path),
+            projections_data_path=Path(config_ChestXray.projections_path),
+            reports_data_path=Path(config_ChestXray.reports_path),
+            processed_data_output_path=Path(
+                config_ChestXray.processed_data_output_path),
+            processed_first=config_ChestXray.processed_first
         )
         return data_processor_config
 
-    def get_training_config(self) -> TrainingConfig:
+    def get_ROCO_data_processing_config(self) -> ROCODataProcessingConfig:
+        config_roco = self.config.ROCO_data_processing_config
+        create_directories([Path(config_roco.processed_data_path)])
+        data_processor_config = ROCODataProcessingConfig(
+            raw_data_path=Path(config_roco.raw_data_path),
+            processed_data_path=Path(config_roco.processed_data_path),
+            images_base_path=Path(config_roco.images_base_path),
+        )
+        return data_processor_config
+
+    def get_training_config(self) -> [TrainingConfig, ChestXrayTrainingConfig, ROCOTrainingConfig]:
         config = self.config.train_config
         create_directories(
-            [Path(config.metrics_folder_path)])
+            [Path(config.ChestXray.metrics_folder_path), Path(config.ROCO.metrics_folder_path)])
         training_config = TrainingConfig(
-            training_data_path=Path(config.training_data_path),
-            trained_model_output_path=Path(config.trained_model_output_path),
-            metrics_folder_path=Path(config.metrics_folder_path),
-            metrics_path=Path(config.metrics_path),
-            batch_size=config.batch_size,
-            epochs=config.epochs,
-            learning_rate=config.learning_rate,
-            freeze_epochs_gpt=config.freeze_epochs_gpt,
-            freeze_epochs_all=config.freeze_epochs_all,
-            device=config.device
+            learning_rate=config.training_params.learning_rate,
+            epochs=config.training_params.epochs,
+            device=config.training_params.device,
+            batch_size=config.training_params.batch_size,
+            freeze_epochs_gpt=config.training_params.freeze_epochs_gpt,
+            freeze_epochs_all=config.training_params.freeze_epochs_all,
+            general_specific_fine_tuning=config.training_params.general_specific_fine_tuning,
         )
-        return training_config
+
+        ChestXray_config = ChestXrayTrainingConfig(
+            train_data_path=Path(config.ChestXray.train_data_path),
+            val_data_path=Path(config.ChestXray.val_data_path),
+            test_data_path=Path(config.ChestXray.test_data_path),
+            training_data_path=Path(config.ChestXray.training_data_path),
+            roco_trained_model_path=Path(
+                config.ChestXray.roco_trained_model_path),
+            trained_model_output_folder_path=Path(
+                config.ChestXray.trained_model_output_folder_path),
+            trained_model_output_path=Path(
+                config.ChestXray.trained_model_output_path),
+            metrics_folder_path=Path(config.ChestXray.metrics_folder_path),
+            metrics_path=Path(config.ChestXray.metrics_path),
+            processed_data_path=Path(config.ChestXray.processed_data_path),
+        )
+
+        ROCO_config = ROCOTrainingConfig(
+            train_data_path=Path(config.ROCO.train_data_path),
+            val_data_path=Path(config.ROCO.val_data_path),
+            test_data_path=Path(config.ROCO.test_data_path),
+            trained_model_output_folder_path=Path(
+                config.ROCO.trained_model_output_folder_path),
+            trained_model_output_path=Path(
+                config.ROCO.trained_model_output_path),
+            metrics_folder_path=Path(config.ROCO.metrics_folder_path),
+            metrics_path=Path(config.ROCO.metrics_path),
+            processed_data_path=Path(config.ROCO.processed_data_path),
+        )
+        if not training_config.general_specific_fine_tuning:
+            return training_config, ChestXray_config
+        else:
+            return training_config, ChestXray_config, ROCO_config
 
     def get_model_config(self) -> ModelConfig:
         config = self.config.model_config
--- src/entity/configEntity.py
+++ src/entity/configEntity.py
@@ -3,26 +3,57 @@ from pathlib import Path
 
 
 @dataclass(frozen=True)
-class DataProcessingConfig:
+class ChestXrayDataProcessingConfig:
     processed_data_path: Path
     images_base_path: Path
     projections_data_path: str
     reports_data_path: Path
     processed_data_output_path: Path
+    processed_first: bool
+
+
+@dataclass(frozen=True)
+class ROCODataProcessingConfig:
+    raw_data_path: Path
+    processed_data_path: Path
+    images_base_path: Path
 
 
 @dataclass(frozen=True)
 class TrainingConfig:
-    training_data_path: Path
-    trained_model_output_path: Path
-    metrics_folder_path: Path
-    metrics_path: Path
     batch_size: int
     epochs: int
     learning_rate: float
     freeze_epochs_gpt: int
     freeze_epochs_all: int
     device: str
+    general_specific_fine_tuning: Path
+
+
+@dataclass(frozen=True)
+class ChestXrayTrainingConfig:
+    train_data_path: Path
+    val_data_path: Path
+    test_data_path: Path
+    training_data_path: Path
+    roco_trained_model_path: Path
+    trained_model_output_folder_path: Path
+    trained_model_output_path: Path
+    metrics_path: Path
+    metrics_folder_path: Path
+    processed_data_path: Path
+
+
+@dataclass(frozen=True)
+class ROCOTrainingConfig:
+    train_data_path: Path
+    val_data_path: Path
+    test_data_path: Path
+    trained_model_output_folder_path: Path
+    trained_model_output_path: Path
+    metrics_path: Path
+    metrics_folder_path: Path
+    processed_data_path: Path
 
 
 @dataclass(frozen=True)
